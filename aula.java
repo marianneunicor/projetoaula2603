63c409eaca5d45e9fb08d6eee9bec3edb0e077ef
marianneunicor <108650617+marianneunicor@users.noreply.github.com>
2024-10-30 20:33:40 -0300

    bb

---
+++ aula.java
@@ -0,0 +1,14 @@
+import java.util.Scanner
+
+public class exe01{
+    public static void main (String[] args){
+        int raio, altura;
+        double pi = Math.pi;
+
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Qual a altura do seu cilindro? ");
+        altura = leia.nextInt();
+         
+
+    }
+}
